2d8dc2b039bf851874d85a05850cc8f218aa638b
Kunal0007 <kunal.kv1dr64@gmail.com>
2026-01-11 22:22:25 +0530

    RSS feeds to post logic

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Kunal0007/RSS_Aggregator/internal/database"
 	"github.com/go-chi/chi"
@@ -38,9 +39,12 @@ func main() {
 		log.Fatal("Connection failed to database:", err)
 	}
 
+	db := database.New(conn)
 	apiCfg := apiConfig{
-		DB: database.New(conn),
+		DB: db,
 	}
+
+	go startScraping(db, 10, time.Minute)
 
 	router := chi.NewRouter()
 
